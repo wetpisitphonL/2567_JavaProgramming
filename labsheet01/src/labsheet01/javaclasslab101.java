c2a73974234eb42d95e4fb57fd261f27aaf35ccc
wetpisitphonL <boots02188@gmail.com>
2024-11-27 11:07:48 +0700

    labsheet01

--- labsheet01/src/labsheet01/javaclasslab101.java
+++ labsheet01/src/labsheet01/javaclasslab101.java
@@ -1,10 +1,30 @@
-package labsheet01;
-
+import java.util.*;
+import java.text.*;
 public class javaclasslab101 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		
+		DecimalFormat frm = new DecimalFormat("#,###.00");
+		System.out.print("Input product name:  ");
+		String productName= input.nextLine();
+		
+		System.out.print("Input product unit : ");
+		int productUnit = input.nextInt();
+		
+		System.out.print("Input price per unit : ");
+		float  productPrice = input.nextFloat();
+		System.out.print("_______________________________________");
+		float totalPrice= productUnit * productPrice;
+		System.out.println("Total price is "+frm.format(totalPrice)+" baht.");
+		
+		
+		
+		
+		
+		
 	}
 
 }
