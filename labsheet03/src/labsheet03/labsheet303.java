362871eec6becd25d3d706cd7a21d75e8f697676
wetpisitphonL <boots02188@gmail.com>
2024-12-11 11:15:17 +0700

    labsheet303.java

---
+++ labsheet03/src/labsheet03/labsheet303.java
@@ -0,0 +1,17 @@
+package labsheet03;
+import java.util.*;
+public class labsheet303 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		while(true) {
+			System.out.print("Input a Sentene: ");
+			String St = scan.nextLine();
+			
+		}
+
+	}
+
+}
